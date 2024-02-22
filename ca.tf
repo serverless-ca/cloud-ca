d63086b810f123abf85a087fd3964885de2112aa
Paul Schwarzenberger <paul@celidor.net>
2024-02-22 11:52:44 +0000

    Add public certs and CRL

--- ca.tf
+++ ca.tf
@@ -3,12 +3,15 @@ module "certificate_authority" {
   # source  = "serverless-ca/terraform-aws-ca"
   # version = "1.0.0"
 
-  cert_info_files = ["tls", "revoked", "revoked-root-ca"]
-  issuing_ca_info = local.issuing_ca_info
-  root_ca_info    = local.root_ca_info
+  hosted_zone_domain = var.hosted_zone_domain
+  hosted_zone_id     = data.aws_route53_zone.public.zone_id
+  cert_info_files    = ["tls", "revoked", "revoked-root-ca"]
+  issuing_ca_info    = local.issuing_ca_info
+  root_ca_info       = local.root_ca_info
+  public_crl         = true
 
   providers = {
     aws           = aws
-    aws.us-east-1 = aws.us-east-1 # required even if CloudFront not used
+    aws.us-east-1 = aws.us-east-1 # certificates for CloudFront must be in this region
   }
 }
