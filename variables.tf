4f897724b2912d55dfb06bfb6c3bc9c9b290bab3
Paul Schwarzenberger <paul@celidor.net>
2024-04-23 15:54:52 +0100

    Allow cloud app user to access external S3 bucket

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "hosted_zone_domain" {
   description = "Hosted Zone Domain"
   default     = "certs.cloud-ca.com" # Change to subdomain hosted zone for CRL publication within same AWS account
 }
+
+variable "s3_aws_principals" {
+  description = "AWS principals allowed to get certificate info from external CA S3 bucket"
+  default     = []
+}
