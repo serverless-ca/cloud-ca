9bfc32cf326fd22b55d369978243545c6cfc458d
Paul Schwarzenberger <paul@celidor.net>
2025-08-22 16:23:59 +0100

    Upgrade CA

--- ca.tf
+++ ca.tf
@@ -1,6 +1,6 @@
 module "certificate_authority" {
   source  = "serverless-ca/ca/aws"
-  version = "2.0.0"
+  version = "2.1.0"
 
   hosted_zone_domain = var.hosted_zone_domain
   hosted_zone_id     = data.aws_route53_zone.public.zone_id
